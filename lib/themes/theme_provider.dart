f7384bfa2f23ee3cfd13ef146a31c827abdc73f3
AhmedHesham <ahmedhesham200987@gmail.com>
2024-06-24 18:55:51 +0300

    first commit >> Light and dark mode implemented

---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:habits/themes/dark_mode.dart';
+import 'package:habits/themes/light_mode.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightMode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData theme) {
+    _themeData = theme;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightMode) {
+      _themeData = darkMode;
+    } else {
+      _themeData = lightMode;
+    }
+    notifyListeners();
+  }
+}
